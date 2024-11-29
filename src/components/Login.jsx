c1aeec9b03f724b4075ffb72a4b302f7ee6ee407
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-29 13:18:46 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,38 @@
 import{Link} from 'react-router-dom';
+
 export default function Login(){
+    const[user,setUser]=useState({username:'' , password:'',error:''});
+
+    const getUserInputs =(e)=>{
+        const {name,value}=e.target;
+        setUser((prev)=>({
+            ...prev,[name]:value,
+        }));
+    }
+
+    const authentication =()=>{
+        if(user.username=== 'admin' && user.password==='password123'){
+            setUser({username:'',password:'',error:''});
+            Navigate('/dashbord');
+        }else{
+            setUser({'error':'Invalid username or password'});
+        }
+    }
     return(
         <div>
-            <h1>Login Here</h1>
+            <h1 style={{textAlign:'center'}}>Login Here</h1>
 
            <table>
             <tr>
                 <td>Username:</td>
-                <td><input type='text' name='username' onChange=''></input></td>
+                <td><input type='text' name='username' onChange='getUserInputs'></input></td>
             </tr>
             <tr>
                 <td>Password:</td>
-                <td><input type='password' name='password' onChange=''></input></td>
+                <td><input type='password' name='password' onChange='getUserInputs'></input></td>
             </tr>
             <tr>
-                <td colSpan={2} style={{color:'red'}}></td>
+                <td colSpan={2} style={{color:'red'}}>{user.error}</td>
                 
             </tr>
            </table>
